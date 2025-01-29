7c9ac3ea3bac78bc931e66898d3b434a5a14a1da
Sebastien-cloud <bastien0290@hotmail.fr>
2025-01-29 13:20:30 +0100

    ajustements

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ let etape3 = document.getElementById("etape3");
 let etape4 = document.getElementById("etape4");
 let etape5 = document.getElementById("etape5");**/
 
-
+// la position de l'etape acutlelment affichée
+let courante = 0
 let etapes = document.querySelectorAll("#etapes div")
 let images = document.querySelectorAll ("#images img")
 // pour chacunes des etapes
 etapes.forEach((etape,pos) =>{    
     // => je ecoute le click
     etape.addEventListener("click",()=>{
+        courante= pos
         // deselectionner celle qui est deja active
         let active = document.querySelector(".actives")
         active.classList.remove("actives")
@@ -41,6 +43,33 @@ etapes.forEach((etape,pos) =>{
     */
 
 
+setInterval(()=>{
+    // masquer l'etape courante
+    let active = document.querySelector(".actives")
+    active.classList.remove("actives")
+    // masquer l'image courante
+    let opacity = document.querySelector(".splineActive")
+    opacity.classList.remove("splineActive")
 
+    // on passe a la position suivante
+    courante++;
+    if(courante> images.length-1){
+        courante = 0   
+    }
+    // affifcher letape suivante
+    etapes[courante].classList.add("actives")
+    // afficher l'image suivante
+    images[courante].classList.add("splineActive")
+},4000)
+
+
+    document.addEventListener('scroll', () => {
+        const header = document.querySelector('header');
+        if (window.scrollY > 600){
+            header.classList.add('scrolled')
+        }else{
+            header.classList.remove('scrolled')
+        }
+    })
 
 
